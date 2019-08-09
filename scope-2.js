d313e37bb1a93b59542bfb460de9a93f05b3a69a
ismaelwolf <ismour.ol@gmail.com>
2019-08-09 12:44:48 -0300

    New archive about variable scope

---
+++ scope-2.js
@@ -0,0 +1,18 @@
+//Global
+  //local (parent scope)
+    //local
+  //local  
+
+let name ='Andrew'
+if (true){
+    let name = 'Mike'
+    
+    if (true){
+        name = 'Jen'
+        console.log(name)
+    }
+}
+
+if (true){
+        console.log(name)
+}
