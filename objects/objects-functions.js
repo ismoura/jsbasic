49986f64020ccfdd1950b028fca5d927bc8c64aa
itsmael <ismour.ol@gmail.com>
2019-12-11 22:59:39 -0300

    Add challenge area

--- objects/objects-functions.js
+++ objects/objects-functions.js
@@ -24,11 +24,18 @@ let otherBookSummary = getSummary(otherBook)
 console.log(bookSummary.pageCountSummary)
 
 
+//challenge Area
 
+let convertFahrenheit = function (fahrenheit){
+    return{
+        fahrenheit: fahrenheit,
+        kelvin: (fahrenheit + 459.67) * (5 / 9),
+        celsius: (fahrenheit - 32) * (5 / 9)
+    }
+}
 
-
-
-
+let temps = convertFahrenheit(74)
+console.log (temps)
 
 
 
