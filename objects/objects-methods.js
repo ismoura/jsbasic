e3ab07df9f571b14656c24dece8963f8d6d0d7ca
ismaelwolf <ismour.ol@gmail.com>
2019-12-13 14:41:39 -0300

    new archive for the new class about methods

---
+++ objects/objects-methods.js
@@ -0,0 +1,29 @@
+let restaurant = {
+    name: 'ASB',
+    guestCapacity: 75,
+    guestCount: 0,
+    //this is a method
+    checkAvailability: function(partySize) {
+     //console.log(this)
+     //   console.log(this.guestCapacity)
+
+        let seatsLeft =  this.guestCapacity - this.guestCount
+        return partySize <= seatsLeft//true
+        },
+    seatParty: function(partySize){    
+        this.guestCount = this.guestCapacity + partySize
+        return partySize <= this.guestCount
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCapacity - partySize
+        return partySize >= this.guestCount
+    }
+
+}
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvailability(4))
+
+//let status = restaurant.checkAvailability(4)
+//console.log(status)
